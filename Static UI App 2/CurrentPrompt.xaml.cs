eceb99a58416107f328c9b96824089e68c9e40bd
braydenliinamaa <braydenliinamaa@gmail.com>
2024-12-09 18:36:45 -0500

    Added Current Prompt and Updated MainPage

---
+++ Static UI App 2/CurrentPrompt.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace Static_UI_App_2
+{
+    public partial class CurrentPrompt : ContentPage
+    {
+        String[] characters = { "cat", "dog", "bird", "fish", "person"};
+        String[] actions = { "running", "dancing", "doing a backflip", "playing an instrument", "just standing there, menacingly" };
+        String[] locations = { "in the middle of a city", "in a small village", "in the forest", "in a park", "on the street" };
+
+        public CurrentPrompt()
+        {
+            InitializeComponent();
+        }
+        private String RandomizePrompt()
+        {
+            Random random = new Random();
+            int x = random.Next(0, characters.Length);
+            int y = random.Next(0, actions.Length);
+            int z = random.Next(0, locations.Length);
+            return $"Todays Theme: Draw a {characters[x]}" 
+                + ((random.Next(3) == 1) ? $" {actions[y]} " : " ") + ((random.Next(3) == 1) ? $"{locations[z]}." : ".");
+        }
+
+        private async void ChangePage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+
+}
--- Static UI App 2/MainPage.xaml.cs
+++ Static UI App 2/MainPage.xaml.cs
@@ -2,23 +2,20 @@
 {
     public partial class MainPage : ContentPage
     {
-        String[] characters = { "cat", "dog", "bird", "fish", "person"};
-        String[] actions = { "running", "dancing", "doing a backflip", "playing an instrument", "just standing there, menacingly" };
-        String[] locations = { "in the middle of a city", "in a small village", "in the forest", "in a park", "on the street" };
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void GenerateTheme()
+        private async void ChangePage(object sender, EventArgs e)
         {
-            Random random = new Random();
-            int x = random.Next(0, characters.Length);
-            int y = random.Next(0, actions.Length);
-            int z = random.Next(0, locations.Length);
-            String currentTheme = $"Todays Theme: Draw a {characters[x]}" 
-                + ((random.Next(3) == 1) ? $" {actions[y]} " : " ") + ((random.Next(3) == 1) ? $"{locations[z]}." : ".");
+            if (sender is Button button)
+            {
+                if (button == DrawPromptBtn)
+                    await Navigation.PushAsync(new CurrentPrompt());
+                else
+                    await Navigation.PushAsync(new MainPage());
+            }
         }
     }
 
